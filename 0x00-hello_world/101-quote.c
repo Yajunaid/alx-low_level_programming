0c35c505a93324f288e192d2ca73e92e0cc3c8d7
Yahaya Abubakar <yahayaabubakarjunaidu@gmail.com>
2022-09-13 01:54:23 +0100

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: and program
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2020-09-13";
+
+	fprintf(stderr, "%\n", n);
+
+	return (1);
+}
